2bd4c66bbc97535fffd983945d4d132f963b97de
Cnr007 <823411732@qq.com>
2019-09-22 15:46:01 -0400

    Initial Commit
    Analysis the data(pictures and text);

---
+++ analysis.py
@@ -0,0 +1,17 @@
+# retrun the list of top 10 popular pictures of the keyword
+# Input: arguments
+# Output: List(Dict), the dictionary contains url of the original tweet,
+#         expanded url (web page containing the video/photo), media url (https).
+def get_picture_list(arguments):
+	arguments[entities]
+
+# return the overall sentiment of the keyword (float range from -1 to 1)
+# Input: arguments
+# Output: ([-1, 1]+1)*50 --> [0, 100]
+def get_keyword_sentiment(arguments):
+
+#
+def get_count(keywords):
+
+#
+def get_single_sentiment(keywors):
